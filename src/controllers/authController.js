fe64a7506d31d870f954d1fbf3babc1c2cfa9f8f
Akshat Biniwale <akshatbiniwale@gmail.com>
2025-05-18 15:53:07 +0530

    bug fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,6 @@ const register = async (req, res) => {
 		const hashedPassword = await bcrypt.hash(password, 10);
 		const user = await User.create({
 			username,
-			password: hashedPassword,
 			role,
 		});
 		res.status(201).json({ message: "User registered successfully", user });
